ed7065c41595471b9d051580b2d0d9ff937432ce
Antonio MM <ant.moreno.martin@gmail.com>
2023-03-28 13:40:19 +0200

    Ploteo de 2020-2023 ready to start el forecasting

--- download_data.py
+++ download_data.py
@@ -5,7 +5,10 @@ import pandas as pd
 import numpy as np
 from datetime import date
 from scrap_functions import *
+import shutil
 
+#### Select year to download ###
+year = '2023'
 url = "https://sedeaplicaciones.minetur.gob.es/shpcarburantes/"
 webdriver = scrap(url)
 
@@ -13,7 +16,8 @@ webdriver = scrap(url)
 webdriver.pick_temporalidad('Mensual')
 
 ### Insertar Tiempo Inicial y Final ###
-webdriver.insert_dates('20/03/2021', '20/03/2022')
+#webdriver.insert_dates('01/01/' + year, '31/12/' + year)
+webdriver.insert_dates('01/01/' + year, '27/03/' + year)
 
 ## Insertar la busqueda con detalles
 webdriver.variables_to_plot(['Gasolinera', 'Madrid', 'Madrid', 'Madrid', 'Madrid', 'Alcampo', 'Gasolina 98 E5', 'G'])
@@ -23,6 +27,12 @@ webdriver.click_element('/html/body/form/div[3]/div[3]/div/div[1]/fieldset[2]/di
 
 # Descargar el Excel
 webdriver.click_element('/html/body/form/div[3]/div[3]/div/div[1]/fieldset[2]/div[1]/div[1]/div/table/tbody/tr[2]/td[5]/input[2]')
+
+# Renombrar y mover a la carpeta de source deseada. Siempre se guarda con nombre Data.xls
+file = '/Users/amm/Downloads/Datos.xls'
+new_file = year + '.xls'
+shutil.move(file, "/Users/amm/Documents/Github/Data/Gasolina/source/Madrid, alcampo, gasolina 98_E5/" + new_file)
+
 
 
 
--- working_data.py
+++ working_data.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import glob
+from datetime import datetime
+import plotly.express as px
 import seaborn as sns
 import xgboost as xgb
 
-df = pd.read_excel('/Users/amm/Downloads/Datos.xls')
-df = df.set_index('Fecha')
-df.plot(style='.', figsize=(15, 5), )
 
+input = "/Users/amm/Documents/Github/Data/Gasolina/source/Madrid, alcampo, gasolina 98_E5/"
+files = glob.glob(input + '*.xls')
+
+df = pd.concat([pd.read_excel(file) for file in files], ignore_index=True)
+
+df['Fecha'] = pd.to_datetime(df['Fecha'])
+df.sort_values(by='Fecha', inplace = True)
+
+fig = px.line(df, x = 'Fecha', y = 'Precio', title = 'Gasolina 98 E5 Madrid 2020 - 2023')
+fig.show()
+df
