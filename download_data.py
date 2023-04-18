ccb5f1b0b088b8dbc96804495418e6b11d9aa62d
Antonio MM <ant.moreno.martin@gmail.com>
2023-04-18 14:18:21 +0200

    Probamos MA y naive baselines

--- download_data.py
+++ download_data.py
@@ -7,37 +7,49 @@ import numpy as np
 from datetime import date
 from scrap_functions import *
 import shutil
-
-
 from datetime import date, timedelta
-yesterday = date.today() - timedelta(days=1)
-last_data = yesterday.strftime('%d/%m/%Y')
+import os
 
-#### Select year to download ###
-year = '2023'
-url = "https://sedeaplicaciones.minetur.gob.es/shpcarburantes/"
-webdriver = scrap(url)
+year = 2014
 
-### Insertar Temporalidad ###
-webdriver.pick_temporalidad('Mensual')
+while year>0:
 
-### Insertar Tiempo Inicial y Final ###
-#webdriver.insert_dates('01/01/' + year, '31/12/' + year)
-webdriver.insert_dates('01/01/' + year, last_data)
+    #### To download the last data of this year (it has to be the day before today) ###
+    #yesterday = date.today() - timedelta(days=1)
+    #data = yesterday.strftime('%d/%m/%Y')
 
-## Insertar la busqueda con detalles
-webdriver.variables_to_plot(['Gasolinera', 'Madrid', 'Madrid', 'Madrid', 'Madrid', 'Alcampo', 'Gasolina 98 E5', 'G'])
+    ### To download data for a certain year ###
 
-# Aceptar la busqueda
-webdriver.click_element('/html/body/form/div[3]/div[3]/div/div[1]/fieldset[2]/div/div[3]/fieldset/div[2]/input')
+    data = '31/12/' + str(year)
 
-# Descargar el Excel
-webdriver.click_element('/html/body/form/div[3]/div[3]/div/div[1]/fieldset[2]/div[1]/div[1]/div/table/tbody/tr[2]/td[5]/input[2]')
+    url = "https://sedeaplicaciones.minetur.gob.es/shpcarburantes/"
+    webdriver = scrap(url)
 
-# Renombrar y mover a la carpeta de source deseada. Siempre se guarda con nombre Data.xls
-file = '/Users/amm/Downloads/Datos.xls'
-new_file = year + '.xls'
-shutil.move(file, "/Users/amm/Documents/Github/Data/Gasolina/source/Madrid, alcampo, gasolina 98_E5/" + new_file)
+
+    ### Insertar Temporalidad ###
+    webdriver.pick_temporalidad('Mensual')
+
+    ### Insertar Tiempo Inicial y Final ###
+    #
+    webdriver.insert_dates('01/01/' + str(year), data)
+
+    ## Insertar la busqueda con detalles
+    webdriver.variables_to_plot(['Gasolinera', 'Madrid', 'Madrid', 'Madrid', 'Madrid', 'Alcampo', 'Gasolina 98 E5', 'G'])
+
+    # Aceptar la busqueda
+    webdriver.click_element('/html/body/form/div[3]/div[3]/div/div[1]/fieldset[2]/div/div[3]/fieldset/div[2]/input')
+
+    # Descargar el Excel
+    webdriver.click_element('/html/body/form/div[3]/div[3]/div/div[1]/fieldset[2]/div[1]/div[1]/div/table/tbody/tr[2]/td[5]/input[2]')
+
+    # Renombrar y mover a la carpeta de source deseada. Siempre se guarda con nombre Data.xls
+    file = '/Users/amm/Downloads/Datos.xls'
+    
+    new_file = str(year) + '.xls'
+    
+    #os.rename(file,"/Users/amm/Documents/Github/Data/Gasolina/source/Madrid_alcampo_gasolina_98_E5/" + new_file)
+    #shutil.move(file, "/Users/amm/Documents/Github/Data/Gasolina/source/Madrid_alcampo_gasolina_98_E5/" + new_file)
+    year-=1
 
 
 
--- working_data.py
+++ working_data.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import xgboost as xgb
 
 # Leer y gather data
-input = "/Users/amm/Documents/Github/Data/Gasolina/source/Madrid, alcampo, gasolina 98_E5/"
+input = "/Users/amm/Documents/Github/Data/Gasolina/source/Madrid_alcampo_gasolina_98_E5/"
 files = glob.glob(input + '*.xls')
 
 df = pd.concat([pd.read_excel(file) for file in files], ignore_index=True)
@@ -61,14 +61,110 @@ ADF_result = adfuller(df.Precio)
 print(f'ADF Statistic: {ADF_result[0]}')
 print(f'p-value: {ADF_result[1]}')
 
-plot_acf(df.Precio, lags=10) # Vemos como hay una relación linela en las muestras y por tanto, es no estacionario. Dentro del confidence interval se considera que es como tener 0
+plot_acf(df.Precio, lags=12) # Vemos como hay una relación linela en las muestras y por tanto, es no estacionario. Dentro del confidence interval se considera que es como tener 0
 
 # Como el p valor es 0.79 y es mayor que 0.05, no podemos rechazar la null hyphotesis y por tanto es no estacionacionaria. Por ende, tennemos que diferenciar.
 
 diff_gasolina = np.diff(df['Precio'], n = 1)
 print(f'ADF Statistic: {diff_gasolina[0]}')
 print(f'p-value: {diff_gasolina[1]}')
-plot_acf(diff_gasolina, lags=10) # Vemos como tenemos coeficientes significativos después de 0, esto q  uiere decir que es un random walk claramente
+plot_acf(diff_gasolina, lags = 12) # Vemos como tenemos coeficientes significativos después de 0, esto q  uiere decir que es un random walk claramente
 
-# Podemos asertar que estamos frente a un random walk
+ADF_result = adfuller(diff_gasolina)
+print(f'ADF Statistic: {ADF_result[0]}')
+print(f'p-value: {ADF_result[1]}')
 
+
+####### Probamos Moving Average
+
+
+df_diff = pd.DataFrame({'diff_gasolina': diff_gasolina})
+
+train = df_diff[:int(0.7*len(df_diff))]
+test = df_diff[int(0.7*len(df_diff)):]
+
+
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+def rolling_forecast(df: pd.DataFrame, train_len: int, horizon: int, window: int, method: str) -> list:
+    
+    total_len = train_len + horizon
+    
+    if method == 'mean':
+        pred_mean = []
+        
+        for i in range(train_len, total_len, window):
+            mean = np.mean(df[:i].values)
+            pred_mean.extend(mean for _ in range(window))
+
+        return pred_mean
+
+    elif method == 'last':
+        pred_last_value = []
+        
+        for i in range(train_len, total_len, window):
+            last_value = df[:i].iloc[-1].values[0]
+            pred_last_value.extend(last_value for _ in range(window))
+            
+        return pred_last_value
+    
+    elif method == 'MA':
+        pred_MA = []
+        
+        for i in range(train_len, total_len, window):
+            model = SARIMAX(df[:i], order=(0,0,1))
+            res = model.fit(disp=False)
+            predictions = res.get_prediction(0, i + window - 1)
+            oos_pred = predictions.predicted_mean.iloc[-window:]
+            pred_MA.extend(oos_pred)
+            
+        return pred_MA
+
+
+pred_df = test.copy()
+
+TRAIN_LEN = len(train)
+HORIZON = len(test)
+WINDOW = 1
+
+
+pred_mean = rolling_forecast(df_diff, TRAIN_LEN, HORIZON, WINDOW, 'mean')
+pred_last_value = rolling_forecast(df_diff, TRAIN_LEN, HORIZON, WINDOW, 'last')
+pred_MA = rolling_forecast(df_diff, TRAIN_LEN, HORIZON, WINDOW, 'MA')
+
+pred_df['pred_mean'] = pred_mean
+pred_df['pred_last_value'] = pred_last_value
+pred_df['pred_MA'] = pred_MA
+
+
+####----------------------###
+import plotly.graph_objects as go
+
+def draw_trace(data,legend_name,len_test=len(test)):
+    trace = go.Scatter(
+    x = df['Fecha'][-len_test:],
+    y = data, name = legend_name)
+    
+    return trace   
+
+trace1 =  go.Scatter(
+    x = df['Fecha'][1:],
+    y = df_diff['diff_gasolina'], name = '')
+
+trace2 = draw_trace(pred_df['diff_gasolina'], 'Actual')
+trace3 = draw_trace(pred_df['pred_mean'], 'Mean')
+trace4 = draw_trace(pred_df['pred_last_value'], 'last')
+trace5 = draw_trace(pred_df['pred_MA'], 'MA')
+
+traces = [trace1, trace2, trace3, trace4, trace5]
+fig2 = go.Figure(data=traces)
+fig2.show()
+
+
+from sklearn.metrics import mean_squared_error
+
+mse_mean = mean_squared_error(pred_df['diff_gasolina'], pred_df['pred_mean'])
+mse_last = mean_squared_error(pred_df['diff_gasolina'], pred_df['pred_last_value'])
+mse_MA = mean_squared_error(pred_df['diff_gasolina'], pred_df['pred_MA'])
+
+print(mse_mean, mse_last, mse_MA)
